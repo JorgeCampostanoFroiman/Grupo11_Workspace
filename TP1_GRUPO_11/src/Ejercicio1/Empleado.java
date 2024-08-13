41de04a0fda66123fb7b2ac6615913eaeca6b398
coutnico <nicoo_rodrii@hotmail.com>
2024-08-13 20:33:58 -0300

    Development of point 4

--- TP1_GRUPO_11/src/Ejercicio1/Empleado.java
+++ TP1_GRUPO_11/src/Ejercicio1/Empleado.java
@@ -2,10 +2,19 @@ package Ejercicio1;
 
 public class Empleado {
 
-	private int id;
+	private final int id;
 	
 	private String nombre;
 	
 	private int edad;
 	
+	static int contador_Empleados = 999;
+	
+	
+	public Empleado() 
+	{
+		contador_Empleados++;
+		
+		id = contador_Empleados;
+	}
 }
