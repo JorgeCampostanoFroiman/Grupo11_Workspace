ff3d9dfe2d04afdbb527740c571605b8a1c19b37
marianrp09 <marian_rp09@hotmail.com>
2024-08-13 23:06:01 -0300

    Added GETTERS & SETTERS (Empleado Class)

--- TP1_GRUPO_11/src/Ejercicio1/Empleado.java
+++ TP1_GRUPO_11/src/Ejercicio1/Empleado.java
@@ -10,11 +10,35 @@ public class Empleado {
 	
 	private static int contador_Empleados = 999;
 	
-	
 	public Empleado() 
 	{
 		contador_Empleados++;
 		
 		id = contador_Empleados;
+		
+		
 	}
+
+	//GETTERS & SETTERS
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	
 }
