05ae35edb56b4ea63aafd7a00d44c5b0886113c7
BambuBlu <55064080+BambuBlu@users.noreply.github.com>
2024-08-20 10:57:34 -0300

    Create Profesor.java

---
+++ TP1_GRUPO_11/src/Ejercicio1/Profesor.java
@@ -0,0 +1,12 @@
+package Ejercicio1;
+
+public class Profesor extends Empleado {
+	private String cargo;
+	private int antiguedadDocente;
+	
+	public Profesor() {
+		super();
+		this.cargo = "sin cargo";
+		this.antiguedadDocente = 0;
+	}
+}
