37b6536ceebfad3420964d0369edf31703e2acff
coutnico <nicoo_rodrii@hotmail.com>
2024-09-04 23:15:37 -0300

    Update Archivo.java

--- TP3_GRUPO_11/src/ejercicio1/Archivo.java
+++ TP3_GRUPO_11/src/ejercicio1/Archivo.java
@@ -15,56 +15,53 @@ public class Archivo {
 
 	public boolean existe() {
 		File archivo = new File(ruta);
-		if(archivo.exists()) 
+		if (archivo.exists())
 			return true;
 		return false;
+	}
+
+	// Metodo para leer personas desde archivo
+	public Set<Persona> leerPersonas() {
+		Set<Persona> personas = new TreeSet<>((p1, p2) -> p1.getApellido().compareTo(p2.getApellido()));
+
+		if (!existe()) {
+			System.out.println("El archivo no existe: " + ruta);
+			return personas;
 		}
-	
-	//Metodo para leer personas desde archivo
-    public Set<Persona> leerPersonas() {
-    	Set<Persona> personas = new TreeSet<>((p1, p2) -> p1.getApellido().compareTo(p2.getApellido()));
-    	
-        if (!existe()) {
-            System.out.println("El archivo no existe: " + ruta);
-            return personas;
-        }
-    	
-        try (BufferedReader buffer = new BufferedReader(new FileReader(ruta))) {
-            String linea;
-            while ((linea = buffer.readLine()) != null) {
-                String[] datos = linea.split("-");
-                if (datos.length == 3) {
-                    String nombre = datos[0].trim();
-                    String apellido = datos[1].trim();
-                    String dniStr = datos[2].trim();
 
-                    
-                    	Persona aux  = new Persona();
-                    	aux.setNombre(nombre);
-                    	aux.setApellido(apellido);
-                    	
-                    	//Si el dni es valido, lo tranformo para seteralo y agrego a la lista
-                    	if (DniValidator.verificarDniInvalido(dniStr)) {
-                    		aux.setDni(Integer.parseInt(dniStr));
-                    		personas.add(aux);
-                    	}
-                    	
-                    	if (personas.isEmpty())
-                    		System.out.println("No se han encontrado personas guardadadas en el archivo");
-                    	
-                    	
-                
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		try (BufferedReader buffer = new BufferedReader(new FileReader(ruta))) {
+			String linea;
+			while ((linea = buffer.readLine()) != null) {
+				String[] datos = linea.split("-");
+				if (datos.length == 3) {
+					String nombre = datos[0].trim();
+					String apellido = datos[1].trim();
+					String dniStr = datos[2].trim();
 
-        return personas;
-    }
-	
-	//getters and setters
-	
+					Persona aux = new Persona();
+					aux.setNombre(nombre);
+					aux.setApellido(apellido);
+
+					// Si el dni es valido, lo tranformo para seteralo y agrego a la lista
+					if (DniValidator.verificarDniInvalido(dniStr)) {
+						aux.setDni(Integer.parseInt(dniStr));
+						personas.add(aux);
+					}
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		if (personas.isEmpty())
+			System.out.println("No se han encontrado personas guardadadas en el archivo");
+		
+		return personas;
+	}
+
+	// getters and setters
+
 	public String getRuta() {
 		return ruta;
 	}
