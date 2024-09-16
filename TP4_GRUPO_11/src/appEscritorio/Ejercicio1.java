b89d6819b3bfc7233eced1c485ce90fae1732ad1
coutnico <nicoo_rodrii@hotmail.com>
2024-09-16 00:09:55 -0300

    Add more resolutions in exercise 1

--- TP4_GRUPO_11/src/appEscritorio/Ejercicio1.java
+++ TP4_GRUPO_11/src/appEscritorio/Ejercicio1.java
@@ -2,35 +2,100 @@ package appEscritorio;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Ejercicio1 extends JFrame {
-	private static final long serialVersionUID = 1L; 
-	
-    private JTextField txtNombre, txtTelefono, txtEmail;
+	private static final long serialVersionUID = 1L;
 
-    public Ejercicio1() {
-        setTitle("Ejercicio 1 - Datos de Contacto");
-        setSize(400, 300);
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setLayout(new GridLayout(5, 2));
+	private JTextField txtNombre, txtApellido, txtTelefono;
+	private JTextField txtFechaNac;
 
-        // Campos de texto
-        txtNombre = new JTextField();
-        txtTelefono = new JTextField();
-        txtEmail = new JTextField();
+	public Ejercicio1() {
+		setTitle("Ejercicio 1 - Datos de Contacto");
+		setSize(450, 400);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        // Etiquetas
-        add(new JLabel("Nombre:"));
-        add(txtNombre);
-        add(new JLabel("Teléfono:"));
-        add(txtTelefono);
-        add(new JLabel("Email:"));
-        add(txtEmail);
-        
-     // Botón Mostrar
-        JButton btnMostrar = new JButton("Mostrar");
-        add(btnMostrar);
-    }
-        
+		// Codigo para responsivamente centrar la ventana en cualquier resolucion
 
+		// Obtener las dimensiones de la pantalla
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+		// Obtener el tamaño de la ventana
+		Dimension frameSize = getSize();
+
+		// Calcular la posición para centrar la ventana
+		int x = (screenSize.width - frameSize.width) / 2;
+		int y = (screenSize.height - frameSize.height) / 2;
+
+		setLocation(x, y);
+
+		// Campos de texto
+		txtNombre = new JTextField();
+		txtNombre.setBounds(142, 43, 217, 25);
+		txtApellido = new JTextField();
+		txtApellido.setBounds(142, 89, 217, 25);
+		txtTelefono = new JTextField();
+		txtTelefono.setBounds(142, 135, 217, 25);
+		getContentPane().setLayout(null);
+
+		// Etiquetas
+		JLabel label = new JLabel("Nombre:");
+		label.setBounds(64, 45, 68, 20);
+		getContentPane().add(label);
+		getContentPane().add(txtNombre);
+		JLabel lblApellido = new JLabel("Apellido:");
+		lblApellido.setBounds(64, 91, 68, 20);
+		getContentPane().add(lblApellido);
+		getContentPane().add(txtApellido);
+		JLabel lblTelefono = new JLabel("Telefono:");
+		lblTelefono.setBounds(64, 135, 46, 25);
+		getContentPane().add(lblTelefono);
+		getContentPane().add(txtTelefono);
+
+		// Botón Mostrar
+		JButton btnMostrar = new JButton("Mostrar");
+		btnMostrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (txtApellido.getText().trim() == "" || txtNombre.getText().trim() == ""
+						|| txtTelefono.getText().trim() == "" || txtFechaNac.getText().trim() == "") {
+					PintarTxtBoxIncompletos();
+				} else {
+
+				}
+			}
+		});
+		btnMostrar.setBounds(154, 230, 122, 34);
+		getContentPane().add(btnMostrar);
+
+		txtFechaNac = new JTextField();
+		txtFechaNac.setBounds(142, 179, 217, 25);
+		getContentPane().add(txtFechaNac);
+
+		JLabel lblFechaNac = new JLabel("Fecha Nac:");
+		lblFechaNac.setBounds(64, 179, 76, 25);
+		getContentPane().add(lblFechaNac);
+
+		JLabel lblLosDatosIngresados = new JLabel("Los datos ingresados fueron: ");
+		lblLosDatosIngresados.setBounds(23, 325, 401, 14);
+		getContentPane().add(lblLosDatosIngresados);
+	}
+
+	private void PintarTxtBoxIncompletos() {
+		if (txtNombre.getText().trim() == "") {
+
+		}
+
+		if (txtApellido.getText().trim() == "") {
+
+		}
+
+		if (txtTelefono.getText().trim() == "") {
+
+		}
+
+		if (txtFechaNac.getText().trim() == "") {
+
+		}
+	}
 }
--- TP4_GRUPO_11/src/appEscritorio/Ventana.java
+++ TP4_GRUPO_11/src/appEscritorio/Ventana.java
@@ -5,48 +5,65 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
 import java.awt.Font;
 
+public class Ventana extends JFrame {
 
-public class Ventana extends JFrame{
+	private static final long serialVersionUID = 1L;
 
-	private static final long serialVersionUID = 1L; 
-	
 	private JButton botonEjercicio1 = new JButton();
 	private JButton botonEjercicio2 = new JButton();
 	private JButton botonEjercicio3 = new JButton();
 
-	public Ventana(){
-		 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 setBounds(350,350,500,400);
-		 setTitle(" ");
-		 getContentPane().setLayout(null);
-		 
-		 botonEjercicio1.setText("Ejercicio1");
-		 botonEjercicio1.setBounds(164, 113,  128,33);
-		 botonEjercicio1.addActionListener(e -> new Ejercicio1().setVisible(true));
-		 getContentPane().add(botonEjercicio1);
-		 
-		 botonEjercicio2.setText("Ejercicio2");
-		 botonEjercicio2.setBounds(164, 169,  128,33);
-		 getContentPane().add(botonEjercicio2);
-		 
-		 botonEjercicio3.setText("Ejercicio3");
-		 botonEjercicio3.setBounds(164, 224, 128, 33);
-		 getContentPane().add(botonEjercicio3);
-		 
-		 JLabel lblGrupo = new JLabel("GRUPO NRO: 11");
-		 lblGrupo.setFont(new Font("Tahoma", Font.BOLD, 16));
-		 lblGrupo.setBounds(53, 44, 138, 24);
-		 getContentPane().add(lblGrupo);
-		 
-		 
+	public Ventana() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(500, 400);
+		
+		//Codigo para responsivamente centrar la ventana en cualquier resolucion 
+		
+		//Obtener las dimensiones de la pantalla
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		
+		// Obtener el tamaño de la ventana
+		Dimension frameSize = getSize();
+
+		// Calcular la posición para centrar la ventana
+		int x = (screenSize.width - frameSize.width) / 2;
+		int y = (screenSize.height - frameSize.height) / 2;
+
+		setLocation(x, y);
+		setTitle(" ");
+		getContentPane().setLayout(null);
+
+		botonEjercicio1.setText("Ejercicio1");
+		botonEjercicio1.setBounds(164, 113, 128, 33);
+		botonEjercicio1.addActionListener(e -> new Ejercicio1().setVisible(true));
+		getContentPane().add(botonEjercicio1);
+
+		botonEjercicio2.setText("Ejercicio2");
+		botonEjercicio2.setBounds(164, 169, 128, 33);
+		getContentPane().add(botonEjercicio2);
+
+		botonEjercicio3.setText("Ejercicio3");
+		botonEjercicio3.setBounds(164, 224, 128, 33);
+		getContentPane().add(botonEjercicio3);
+
+		JLabel lblGrupo = new JLabel("GRUPO NRO: 11");
+		lblGrupo.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblGrupo.setBounds(53, 44, 138, 24);
+		getContentPane().add(lblGrupo);
+
 	}
+
 	public void cambiarVisibilidad(boolean estado) {
-		if (estado == true) {  
-		 setVisible(true);
-		} else {  setVisible(false);}
-			;
-	}	
-}	
+		if (estado == true) {
+			setVisible(true);
+		} else {
+			setVisible(false);
+		}
+		;
+	}
+}
